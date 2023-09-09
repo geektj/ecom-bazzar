eba12d8c3232db0afeccbd6fe7d6775cb02b5485
Tushar Jain <tusharj968@gmail.com>
2023-09-09 19:01:23 +0530

    products card

--- src/components/Products.js
+++ src/components/Products.js
@@ -19,7 +19,7 @@ const Products = ({products}) => {
       </div>
       <div className="max-w-screen-xl mx-auto py-10 grid grid-cols-4 gap-10">
         {products.map((item) => (
-          <ProductsCard  key={item._id} />
+          <ProductsCard  key={item._id} product={item} />
         ))}
       </div>
     </div>
